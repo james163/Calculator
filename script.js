0f5cca4f18559010f25778540eb74988db2d65aa
James Kimani <jkimani163@gmail.com>
2020-07-10 09:53:42 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+  }
+  // Displays the entered value on screen.
+  function liveScreen(value) {
+    document.getElementById("result").value += value;
+  }
+  // Swaps the style sheet in order to  achieve dark mode.
+  function switchTheme() {
+    let darkMode = document.getElementById("dark-mode");
+    let theme = document.getElementById("theme");
+    if (theme.getAttribute("href") == "light.css") {
+      theme.href = "dark.css";
+      darkMode.innerHTML = "Light Mode 🌙";
+    } else {
+      theme.href = "light.css";
+      darkMode.innerHTML = "Dark Mode 🌙";
+    }
+  }
